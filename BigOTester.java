eb884c6fbf5e7490a2d8665f378363154f2d24fe
Jamion Williams <JamionW@gmail.com>
2018-09-20 14:04:50 -0400

    Update BigOTester.java
    Last version was broken, badly. This actually works.

--- BigOTester.java
+++ BigOTester.java
@@ -7,16 +7,18 @@ public class BigOTester {
 		
 		for (int i=1; i<=n; i++) {
 			for (int j=1; j<=(i*i); j++) {
-/*				if (j % i == 0) {
+				if (j % i == 0) {
 					for (int k = 1; k<=j; k++) {
 						//System.out.println("i is " + i);
 						//System.out.println("j is " + j);
 						//System.out.println(sum);
 						sum++;
 					}
-*/			sum++;	}
+				}
 			}
+			
 			System.out.println("For iteration " + i + ", there were " + sum + " operations.");
+
 			sum = 0;
 		}
 	}
